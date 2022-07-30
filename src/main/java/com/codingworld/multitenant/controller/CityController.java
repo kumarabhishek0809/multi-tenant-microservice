d972743acedc5040daf37849d394e1954a702195
Kumar Abhishek <mail2kumar.2021@gmail.com>
2022-07-30 17:11:35 +0400

    final push
    final push

--- src/main/java/com/codingworld/multitenant/controller/CityController.java
+++ src/main/java/com/codingworld/multitenant/controller/CityController.java
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codingworld.multitenant.entity.City;
 import com.codingworld.multitenant.service.CityService;
 
-@RestController(value = "/api")
+@RestController
+@RequestMapping("/api/v1/")
 public class CityController {
 
 	@Autowired
--- src/main/java/com/codingworld/multitenant/entity/City.java
+++ src/main/java/com/codingworld/multitenant/entity/City.java
@@ -1,38 +1,20 @@
 package com.codingworld.multitenant.entity;
 
-import java.io.Serializable;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name = "city")
-@JsonIgnoreProperties(ignoreUnknown = true)
-@JsonInclude(JsonInclude.Include.NON_NULL)
-//@FilterDef(name = "tenantFilter", parameters = {@ParamDef(name = "tenantId", type = "string")})
-//@Filter(name = "tenantFilter", condition = "tenant_id = :tenantId")
+@Document(collection = "city")
+@NoArgsConstructor
 @Data
-public class City implements Serializable {
-	private static final long serialVersionUID = -4551953276601557391L;
+public class City {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
+	private String id;
 
 	private String name;
 
-	@Column(name = "tenant_id")
 	private String tenantId;
 
-	public static long getSerialVersionUID() {
-		return serialVersionUID;
-	}
 }
